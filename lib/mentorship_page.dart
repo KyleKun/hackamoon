fb87a1d25e264848ab82112f4f642cce748a5997
Emerson Silva <emerson@localhost.localdomain>
2019-09-15 13:00:10 -0300

    Test dev

--- lib/mentorship_page.dart
+++ lib/mentorship_page.dart
@@ -3,12 +3,36 @@ import 'package:flutter/material.dart';
 class MentorshipPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Mentores...'),
-        ),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+         Expanded(
+          flex: 5,
+          child: Padding(padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text("Mentoria!"
+              ) ,
+            ),
+          ),
+         ),
+         Expanded(
+           child: FlatButton(
+              color: Colors.teal,
+              //textColor: Colors.white,
+              disabledColor: Colors.grey,
+              disabledTextColor: Colors.black,
+              padding: EdgeInsets.all(0),
+              splashColor: Colors.blueAccent,
+             onPressed: () {
+               /*...*/
+              },
+            child: Text(
+              "Agendar Mentoria",
+            ),
+            ),
+          )
+      ]
     );
   }
 }
